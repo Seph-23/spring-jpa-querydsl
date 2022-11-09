45505006f087271f9e930c376c4c59f1904647ee
Seph-23 <seongmlyu95@gmail.com>
2022-11-09 15:47:41 +0900

    case

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -6,6 +6,7 @@ import static study.querydsl.entity.QTeam.*;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
@@ -336,7 +337,75 @@ public class QuerydslBasicTest {
       .fetch();
 
     assertThat(result).extracting("age")
-      .containsExactly(40);
+      .containsExactly(30, 40);
+  }
+
+  /**
+   * 나이가 평균 이상인 회원
+   */
+  @Test
+  public void subQueryIn() {
+    QMember memberSub = new QMember("memberSub");
+
+    List<Member> result = queryFactory
+      .selectFrom(member)
+      .where(member.age.in(
+        JPAExpressions
+          .select(memberSub.age)
+          .from(memberSub)
+          .where(memberSub.age.gt(10))
+      ))
+      .fetch();
+
+    assertThat(result).extracting("age")
+      .containsExactly(20, 30, 40);
+  }
+
+  @Test
+  public void selectSubQuery() {
+    QMember memberSub = new QMember("memberSub");
+    List<Tuple> result = queryFactory
+      .select(member.username,
+        JPAExpressions
+          .select(memberSub.age.avg())
+          .from(memberSub)
+      )
+      .from(member)
+      .fetch();
+
+    for (Tuple tuple : result) {
+      System.out.println("tuple = " + tuple);
+    }
+  }
+
+  @Test
+  public void basicCase() {
+    List<String> result = queryFactory
+      .select(member.age
+        .when(10).then("열살")
+        .when(20).then("스무살")
+        .otherwise("기타"))
+      .from(member)
+      .fetch();
+
+    for (String s : result) {
+      System.out.println("s = " + s);
+    }
+  }
+
+  @Test
+  public void complexCase() {
+    List<String> result = queryFactory
+      .select(new CaseBuilder()
+        .when(member.age.between(0, 20)).then("0~20살")
+        .when(member.age.between(21, 30)).then("21~30살")
+        .otherwise("기타"))
+      .from(member)
+      .fetch();
+
+    for (String s : result) {
+      System.out.println("s = " + s);
+    }
   }
 }
 
